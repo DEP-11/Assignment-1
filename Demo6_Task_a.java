5d3699272395008f2645e9bdc1b17d341d166c63
Nilan Dinushka <nilandinushka5@gmail.com>
2023-07-31 01:43:52 +0530

    complete Task 6 - A

---
+++ Demo6_Task_a.java
@@ -0,0 +1,10 @@
+public class Demo6_Task_a{
+    public static void main(String[] args) {
+        String value ="0%";
+        final String Rec = "\033[0m";
+        final String RED ="\033[41m";
+        final String GREEN  ="\033[42m";
+        System.out.printf("%s[     5%s%s%s     ]%s\n",RED,Rec,GREEN,value,Rec);
+        
+    }
+}
