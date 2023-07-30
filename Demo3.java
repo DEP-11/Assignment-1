2330d537b8188faf8a2430914fe7322dba291555
Nilan Dinushka <nilandinushka5@gmail.com>
2023-07-31 00:10:38 +0530

    complete Task 3

---
+++ Demo3.java
@@ -0,0 +1,8 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int countryCode=123;
+        int areaCode=456;
+        int phoneNum=7890;
+        System.out.printf("\033[31m(%d)\033[0m %d-%d\n",countryCode,areaCode,phoneNum);
+    }
+}
